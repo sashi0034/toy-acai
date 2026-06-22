0cbf3b0055dc79b5c79bd03b07e380fa56fd5d37
sashi0034 <satoshi3104.nishi@gmail.com>
2026-06-22 22:52:18 +0900

    DISTANCE_NORMALIZATION_FACTOR 導入

--- sim/hyperparameters.py
+++ sim/hyperparameters.py
@@ -21,5 +21,8 @@ REWARD_DISCOUNT = 0.99
 # 最大シミュレーション時間 (秒)
 MAX_SIMULATION_SECONDS = 15.0
 
+# 観測特徴量の距離を正規化するための係数
+DISTANCE_NORMALIZATION_FACTOR = 1.0 / 500.0
+
 # 観測特徴量の速度を正規化するための係数
 SPEED_NORMALIZATION_FACTOR = 1.0 / 500.0
